bd9de9c9b2093c3b47993a07389271b5ab4a5f65
suvandita <suvanditaswaroop@gmail.com>
2025-02-08 01:40:46 +0530

    Work

--- assignment4.js
+++ assignment4.js
@@ -44,23 +44,9 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-
+/*
 let button=document.querySelectorAll(".portfolio_nav_link");
 let card=document.querySelectorAll(".portfolio_main_one > div");
-
-/*
-for(let j=0;j<button.length;j++){
-    button[j].addEventListener("click",function (){
-        button[j].style.textDecoration="underline";
-        button[j].style.color="rgb(32, 201, 151)";
-        for(let k=0;k<button.length;k++){
-            if(k!=j){
-                button[k].style.textDecoration="none";
-                button[k].style.color="white";
-            }
-        }
-    })
-}*/
 
 button.forEach((j)=>{
     j.addEventListener("click",function (){
@@ -75,12 +61,6 @@ button.forEach((j)=>{
     })
 })
 
-
-
-
-
-
-
 button[0].addEventListener("click",function (){
     for(let i = 0; i < card.length; i++){
         card[i].style.display="block";
@@ -89,36 +69,36 @@ button[0].addEventListener("click",function (){
 button[1].addEventListener("click",function (){
   
         card[0].style.display="block";
-        card[4].style.display="block";
+        card[1].style.display="block";
 
         for(let i = 0; i < card.length; i++){
-            if(i!=0 && i!=4){
+            if(i!=0 && i!=1){
                 card[i].style.display="none";
             }
         }
 })
 button[2].addEventListener("click",function (){
   
-    card[1].style.display="block";
     card[2].style.display="block";
+    card[3].style.display="block";
 
     for(let i = 0; i < card.length; i++){
-        if(i!=1 && i!=2){
+        if(i!=2 && i!=3){
             card[i].style.display="none";
         }
     }
 })
 button[3].addEventListener("click",function (){
   
-    card[3].style.display="block";
-    card[5].style.display="block";
+    card[4].style.display="block";
 
     for(let i = 0; i < card.length; i++){
-        if(i!=3 && i!=5){
+        if(i!=4){
             card[i].style.display="none";
         }
     }
 })
+*/
 
 let nav_list=document.getElementsByClassName("top-nav_link")[0];
 let nav_button=document.getElementsByClassName("top-nav_right2_button")[0];
